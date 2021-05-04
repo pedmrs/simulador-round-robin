670ccd94e038473a72aefd4ee01801b4cbf6c014
Pedro <pemoraes1@gmail.com>
2021-05-03 23:22:49 -0300

    Diminiu tempo máximo para duração de um processo a fim de facilitar a leitura para corrigir bugs

--- processo.h
+++ processo.h
@@ -5,7 +5,7 @@
 
 #define MAX_PROCESSOS 10        // número máximo de processos
 #define MAX_INICIO 7           // limite máximo para inicio de processo
-#define MAX_DURACAO 10          // limite máximo para duração de processo
+#define MAX_DURACAO 5          // limite máximo para duração de processo
 #define DURACAO_IO_DISCO 3      // limite máximo para duração de operação de I/O em disco
 #define DURACAO_IO_FITA 4       // limite máximo para duração de operação de I/O em fita magnética
 #define DURACAO_IO_IMPRESSORA 5 // limite máximo para duração de operação de I/O em impressora
